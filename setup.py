b564f70b46b4fbc470a1eb81b95ad2a7189c6bea
zaltaie <46233852+zaltaie@users.noreply.github.com>
2020-04-13 22:10:17 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+image = cv2.imread('screenshot.png')
+lane_image = np.copy(image)
+canny_image = canny(lane_image)
+plt.imshow(canny_image)
+plt.show()
